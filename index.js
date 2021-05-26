246a862017ab2e3865295e9bc5d80f856d6725de
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-25 20:12:46 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const names = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return names
+} 
+
+function fuzzyMatch(drivers, query) {
+    const queryArray = drivers.filter(driver => driver.substr(0,2) === query)
+    return queryArray
+}
+
+function matchName(drivers, string) { 
+    const nameAndHometowns = drivers.filter(drivers => drivers.name === string)
+    return nameAndHometowns
+}
